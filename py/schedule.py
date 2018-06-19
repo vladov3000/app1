c86b9d797755c9661defc1d6210760d756978281
Vlad <vladov3000@gmail.com>
2018-06-18 18:38:51 -0700

    1

--- py/schedule.py
+++ py/schedule.py
@@ -8,6 +8,37 @@ def readConfig(cfg_file):
             logger.error('Failed to load yaml file:'+str(exc))
             raise exc
 
+def listCourses(d):
+	for subject in d:
+		for course in d[subject]:
+			print (subject,'-',course["name"])
+
+def findDep(subject,course,visited,final):
+	s=d[subject]
+	node={}
+	for i in s:
+		if i["name"]==course:
+			node=i
+	if node=={}:
+		raise Exception("Couldn't find course: "+course)
+
+	final.append(course)
+	if "depends" not in node.keys():
+		return
+	else:
+		for i in node["depends"]:
+			if i not in visited:
+				findDep(subject,i,visited,final)
+		visited.append(course)
+
+
+
 
 d=readConfig("interlake.yml")
-print(d)
+f=[]
+v=[]
+for k in d["math"]:
+	if k["name"] not in v:
+		v.append(k["name"])
+		findDep("math",k["name"],v,f)
+print(f)
