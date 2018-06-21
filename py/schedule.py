95b83f5abad0c1b9f0c98940f9afcd35d9ee0502
Vlad <vladov3000@gmail.com>
2018-06-20 19:30:18 -0700

    l

--- py/schedule.py
+++ py/schedule.py
@@ -1,4 +1,6 @@
 import yaml
+import argparse
+import os
 
 def readConfig(cfg_file):
     with open(cfg_file, 'r') as stream:
@@ -8,12 +10,14 @@ def readConfig(cfg_file):
             logger.error('Failed to load yaml file:'+str(exc))
             raise exc
 
-def listCourses(d):
-	for subject in d:
-		for course in d[subject]:
-			print (subject,'-',course["name"])
+def getCourses(d):
+	l=[]
+	for i in d.keys():
+		l.append({"subject":i,"courses":[ii["name"] for ii in d[i]]})
+	return l
 
-def findDep(subject,course,visited,final):
+def findDep(d,subject,course,visited,final):
+	#print(d)
 	s=d[subject]
 	node={}
 	for i in s:
@@ -22,23 +26,62 @@ def findDep(subject,course,visited,final):
 	if node=={}:
 		raise Exception("Couldn't find course: "+course)
 
-	final.append(course)
+	
 	if "depends" not in node.keys():
+		final.append(course)
 		return
 	else:
 		for i in node["depends"]:
 			if i not in visited:
-				findDep(subject,i,visited,final)
+				findDep(d,subject,i,visited,final)
 		visited.append(course)
+	final.append(course)
+
+def getGraph(d):
+	f=[]
+	v=[]
+	for s in d.keys():
+		for k in d[s]:
+			if k["name"] not in v:
+				v.append(k["name"])
+				findDep(d,s,k["name"],v,f)
+	return f
+
+def getCourse(d,course,subject):
+	s=d[subject]
+	c={}
+	for i in s:
+		if i["name"]==course:
+			c=i
+	if c=={}:
+		raise Exception("Couldn't find course: "+course)
+	return c
+
+def getDep(d,course,subject):
+	f=[]
+	findDep(d,subject,course,[],f)
+	t={"courses":f,"target":course,"subject":subject}
+	return t
 
 
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('func', type=str, help='function to run: courses, prereqs')
+	parser.add_argument("--subject",dest="subject", help="subject of course",type=str,required=False,default="")
+	parser.add_argument("--course",dest="course", help="course name",type=str,required=False,default="")
+	args = parser.parse_args()
+	cfg=readConfig("interlake.yml")
 
+	if args.func=='courses':
+		print(getCourses(cfg))
+	elif args.func=='prereqs':
+		if args.course=='':
+			raise Exception("Course required")
+		if args.subject=='':
+			raise Exception("Subject required")
+		print(getDep(cfg,args.course,args.subject	))
+	else:
+		raise Exception("Not a valid command")
 
-d=readConfig("interlake.yml")
-f=[]
-v=[]
-for k in d["math"]:
-	if k["name"] not in v:
-		v.append(k["name"])
-		findDep("math",k["name"],v,f)
-print(f)
+if __name__ == '__main__':
+    main()
